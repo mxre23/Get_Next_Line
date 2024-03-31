a6cf811db9ff62f614ac9d71990504f16df93a01
Joanna <158480613+mxre23@users.noreply.github.com>
2024-03-31 21:49:35 +0200

    first updates

---
+++ get_next_line_utils.c
@@ -0,0 +1,77 @@
+#include "get_next_line.h"
+
+void	*ft_calloc(size_t count, size_t size)
+{
+	size_t	total_size;
+	void	*ptr;
+
+	total_size = count * size;
+	ptr = malloc(total_size);
+	if (ptr != NULL)
+	{
+		ft_memset(ptr, 0, total_size);
+	}
+	return (ptr);
+}
+
+char	*ft_strchr(const char *s, int c)
+{
+	int	i;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		if (s[i] == (char)c)
+			return ((char *)&s[i]);
+		else
+			i++;
+	}
+	if ((char)c == '\0')
+		return ((char *)&s[i]);
+	else
+		return (NULL);
+}
+
+size_t	ft_strlen(const char *s)
+{
+	size_t	i;
+
+	i = 0;
+	while (s[i] != '\0')
+		i++;
+	return (i);
+}
+
+size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
+{
+	size_t	i;
+
+	i = 0;
+	if (!dstsize)
+		return (ft_strlen(src));
+	while (i < dstsize - 1 && src[i] != '\0')
+	{
+		dst[i] = src[i];
+		i++;
+	}
+	dst[i] = '\0';
+	return (ft_strlen(src));
+}
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	size_t	s1_len;
+	size_t	s2_len;
+	char	*s3;
+
+	if (!s1 || !s2)
+		return (NULL);
+	s1_len = ft_strlen(s1);
+	s2_len = ft_strlen(s2);
+	s3 = (char *) malloc((s1_len + s2_len + 1) * sizeof(char));
+	if (!s3)
+		return (NULL);
+	ft_memcpy(s3, s1, s1_len + 1);
+	ft_strlcat(s3, s2, s1_len + s2_len + 1);
+	return (s3);
+}
