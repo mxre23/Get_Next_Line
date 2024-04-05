9585466528f35ad53a6b47b5222243f715210a66
Joanna <158480613+mxre23@users.noreply.github.com>
2024-04-05 13:26:13 +0200

    Update get_next_line_utils.c

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -71,7 +71,7 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	s3 = (char *) malloc((s1_len + s2_len + 1) * sizeof(char));
 	if (!s3)
 		return (NULL);
-	ft_memcpy(s3, s1, s1_len + 1);
-	ft_strlcat(s3, s2, s1_len + s2_len + 1);
+	ft_strlcpy(s3, s1, s1_len + 1);
+	ft_strlcpy(s3 + s1_len, s2, s2_len + 1);
 	return (s3);
 }
