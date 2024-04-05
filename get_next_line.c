078af0e52987b6ff0ef283507df93cb4cf02dcd1
Joanna <158480613+mxre23@users.noreply.github.com>
2024-04-06 00:13:20 +0200

    updates

--- get_next_line.c
+++ get_next_line.c
@@ -1,16 +1,102 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jmore-oj <jmore-oj@student.42barcel>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/05 18:39:49 by jmore-oj          #+#    #+#             */
+/*   Updated: 2024/04/06 00:10:46 by jmore-oj         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <unistd.h>
 #include "get_next_line.h"
 
 char	*get_next_line(int fd)
 {
-	int	bytes_read;
-	char	*cup_buffer;
+	static char	*stash;
+	char		*line;
+	char		*buffer;
 
-	cup_buffer = ft_calloc (bn + 1, sizeof(char));
-	if (cup_buffer == NULL)
+	line = NULL;
+	buffer = ft_calloc (BUFFER_SIZE + 1, sizeof(char));
+	if (fd < 0 || BUFFER_SIZE <= 0 || read(fd, 0, 0) < 0)
+	{
+		free(stash);
+		free(buffer);
+		stash = NULL;
+		buffer = NULL;
 		return (NULL);
-	bytes_read = read(fd, cup_buffer, bn);
-	if (bytes_read <= 0)
+	}
+	if (!buffer)
 		return (NULL);
-	return (cup_buffer);
+	stash = stash_filling(fd, stash, buffer);
+	if (*stash == 0)
+	{
+		free (stash);
+		return (stash = 0);
+	}
+	line = extract_line(stash, line);
+	stash = extract_new_stash(stash);
+	return (line);
 }
+
+char	*stash_filling(int fd, char *stash, char *buffer)
+{
+	ssize_t	nbytes;
+
+	nbytes = 1;
+	if (!stash)
+		stash = ft_strdup("");
+	while (nbytes > 0)
+	{
+		nbytes = read(fd, buffer, BUFFER_SIZE);
+		if (nbytes == -1)
+		{
+			free (buffer);
+			return (NULL);
+		}
+		buffer[nbytes] = 0;
+		stash = ft_strjoin (stash, buffer);
+		if ((ft_strchr(buffer, '\n')))
+			break ;
+	}
+	free (buffer);
+	return (stash);
+}
+
+char	*extract_new_stash(char *stash)
+{
+	int		len;
+	int		i;
+	char	*new_stash;
+
+	len = 0;
+	i = 0;
+	if (stash == NULL)
+		return (NULL);
+	while (stash[len] != '\n' && stash[len])
+		len++;
+	if (stash[len] == '\n')
+		len++;
+	new_stash = malloc((ft_strlen(stash) - len + 1) * sizeof(char));
+	if (!new_stash)
+		return (NULL);
+	while (stash[len + i])
+	{
+		new_stash[i] = stash[len + i];
+		i++;
+	}
+	free (stash);
+	new_stash[i] = 0;
+	return (new_stash);
+}
+
+char	*extrat_line(char *stash, char *line)
+{
+	int	len;
+	int	i;
+
+	len = 0;
+	i	= 
--- get_next_line.h
+++ get_next_line.h
@@ -1,3 +1,14 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jmore-oj <jmore-oj@student.42barcel>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/05 18:39:08 by jmore-oj          #+#    #+#             */
+/*   Updated: 2024/04/05 20:18:27 by jmore-oj         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
@@ -16,6 +27,5 @@ char	*ft_strjoin(char const *s1, char const *s2);
 size_t	ft_strlen(const char *s);
 size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
 void	*ft_calloc(size_t count, size_t size);
-
 
 #endif
